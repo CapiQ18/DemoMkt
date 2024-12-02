8a2d4278b6430a3b4ccb8106f479e5c39616826a
CapiQ18 <capibara17b@gmail.com>
2024-12-02 12:36:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ app.post('/enviar-datos', (req, res) => {
     console.log('Datos recibidos:', datos);
     res.send('Datos recibidos correctamente');
 });
-
+app.get('/', (req, res) => {
+    res.send('¡Servidor funcionando correctamente!');
+});
 // Iniciar servidor en el puerto 3000
 app.listen(3000, () => {
     console.log('Servidor ejecutándose en http://localhost:3000');
